cc14a96b52abce1c514389881f67727f5163016a
root <crminfo.rp@gmail.com>
2017-08-12 18:07:04 +0600

    Added MongoDB connection

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,70 @@
 module.exports.homelist = function(req, res) {
-  res.render('locations-list', {title: 'Home'});
+  res.render("locations-list", {
+    title: "Loc8r - find places to work with WiFi near you!",
+    pageHeader: {
+      title: "Loc8r",
+      sprapline: "Find places to work with WiFi near you!"
+    },
+    sidebar: "Loc8r helps you find places to work when out and about. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    locations: [{
+      name: "Starcups",
+      address: "125 High Street, Reading, RG6 1PS",
+      rating: 3,
+      facilities: ["Hot drinks", "Food", "Premium WiFi"],
+      distance: "100m"
+    },{
+      name: "Cafe Hero",
+      address: "125 High Street, Reading, RG6 1PS",
+      rating: 4,
+      facilities: ["Hot drinks", "Food", "Premium WiFi"],
+      distance: "200m"
+    },{
+      name: "Burger Queen",
+      address: "125 High Street, Reading, RG6 1PS",
+      rating: 2,
+      facilities: ["Food", "Premium WiFi"],
+      distance: "250m"
+    },]
+  });
 };
 
 module.exports.locationInfo = function(req, res) {
-  res.render('location-info', {title: 'Location info'});
+  res.render("location-info", {
+    title: "Location info",
+    sidebar: "Loc8r helps you find places to work when out and about. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    location: {
+      name: "Starcups",
+      address: "125 High Street, Reading, RG6 1PS",
+      latitude: "51.455041",
+      longitude: "-0.9690884",
+      rating: 3,
+      facilities: ["Hot drinks", "Food", "Premium WiFi"],
+      distance: "100m",
+      openHours: ["Monday - Friday : 7:00am - 7:00pm", "Saturday : 8:00am - 5:00pm", "Sunday : closed"]
+    },
+    reviews: [{
+      author: "Roman Porturovskiy",
+      rating: "4",
+      review: "What a great place. I can't say enough good things about it.",
+      timestamp: "16 July 2013"
+    },{
+      author: "Simon Holmes",
+      rating: "5",
+      review: "What a great place. I can't say enough good things about it.",
+      timestamp: "16 July 2013"
+    },{
+      author: "Charlie Chaplin",
+      rating: "3",
+      review: "It was okay. Coffee wasn't great, but the WiFi was fast.",
+      timestamp: "16 July 2013"
+    }]
+  });
 };
 
 module.exports.addReview = function(req, res) {
-  res.render('location-review-form', {title: 'Add review'});
+  res.render("location-review-form", {
+    title: "Add review",
+    header: "Review ",
+    name: "Starcups"
+  });
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,8 @@
 module.exports.about = function(req, res) {
-  res.render('generic-text', {title: 'About'});
+  res.render('generic-text', {
+    title: 'Loc8r - find places to work with WiFi near you!',
+    header: 'About',
+    subheading: 'Loc8r was created to help people find places to sit down and get a bit of work done.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  });
 };
